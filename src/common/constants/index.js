1085dc11de55eaf35811a8f73e6b382a2fcaf342
what1s1ove <zubko.2007@gmail.com>
2020-11-21 16:07:23 +0200

    module1-task3: + cli constants

---
+++ src/common/constants/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const {USER_ARGV_IDX} = require(`./cli.constants`);
+
+module.exports = {
+  USER_ARGV_IDX,
+};
---
+++ src/common/enums/cli/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const {CliCommandName} = require(`./cli-command-name.enum`);
+
+module.exports = {
+  CliCommandName,
+};
