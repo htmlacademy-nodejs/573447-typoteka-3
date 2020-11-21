746d4079dd5a7bc11122a853754b17d15aefcea0
what1s1ove <zubko.2007@gmail.com>
2020-11-21 17:10:36 +0200

    module1-task3: * helpers root import

---
+++ src/helpers/array/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const {getRandomItem} = require(`./get-random-item.helper`);
+const {getRandomItems} = require(`./get-random-items.helper`);
+const {getShuffledItems} = require(`./get-shuffled-items.helper`);
+
+module.exports = {
+  getRandomItem,
+  getRandomItems,
+  getShuffledItems,
+};
---
+++ src/helpers/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const {getRandomNumber} = require(`./number`);
+const {getRandomItem, getRandomItems, getShuffledItems} = require(`./array`);
+
+module.exports = {
+  getRandomNumber,
+  getRandomItem,
+  getRandomItems,
+  getShuffledItems,
+};
