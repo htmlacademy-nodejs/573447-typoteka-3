4b7889c259c60114b3b2b68364d3961c873f7c5b
what1s1ove <zubko.2007@gmail.com>
2020-11-22 15:45:52 +0200

    module2-task1: * service root file

---
+++ src/service/service.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require(`module-alias/register`);
+const {CliCommandName} = require(`~/common/enums`);
+const {USER_ARGV_IDX, COMMAND_ARGS_IDX} = require(`~/common/constants`);
+const {Cli} = require(`./cli/cli`);
+
+const userArguments = process.argv.slice(USER_ARGV_IDX);
+const [userCommand] = userArguments;
+const commandArguments = userArguments.slice(COMMAND_ARGS_IDX);
+
+if (!userArguments.length || !Cli[userCommand]) {
+  Cli[CliCommandName.VERSION].run(commandArguments);
+}
+
+Cli[userCommand].run(commandArguments);
