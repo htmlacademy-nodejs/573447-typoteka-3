f394e8976f9feb92a628e63a700dc6ae5f901403
what1s1ove <zubko.2007@gmail.com>
2020-11-22 15:45:21 +0200

    module2-task1: * cli enum

--- src/service/cli/cli.js
+++ src/service/cli/cli.js
@@ -4,12 +4,12 @@ const version = require(`./version/version`);
 const help = require(`./help/help`);
 const generate = require(`./generate/generate`);
 
-const cli = {
-  version,
-  help,
-  generate,
+const Cli = {
+  [version.name]: version,
+  [help.name]: help,
+  [generate.name]: generate,
 };
 
 module.exports = {
-  cli,
+  Cli,
 };
