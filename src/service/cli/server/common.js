91e3e73f5c0c2c7ddf1bcf244f845c92e89f0e71
what1s1ove <zubko.2007@gmail.com>
2020-11-24 09:34:33 +0200

    module2-task3: + constants for cli server

---
+++ src/service/cli/server/common.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const DEFAULT_PORT = 3000;
+
+const MOCKS_FILE_PATH = `mocks.json`;
+
+const ServerPath = {
+  ROOT: `/`,
+};
+
+module.exports = {
+  DEFAULT_PORT,
+  MOCKS_FILE_PATH,
+  ServerPath,
+};
