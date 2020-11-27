27ab50722cd7a7721d5b55cf8b7d4866c3637410
what1s1ove <zubko.2007@gmail.com>
2020-11-27 07:55:52 +0200

    module3-task1: + api articles route enum

---
+++ src/common/enums/api/api-articles-route.enum.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const ApiArticlesRoute = {
+  ARTICLE: `/:id`,
+  EDIT: `/edit/:id`,
+  ADD: `/add`,
+  CATEGORY: `/category/:id`,
+};
+
+module.exports = {
+  ApiArticlesRoute,
+};
--- src/common/enums/api/index.js
+++ src/common/enums/api/index.js
@@ -1,7 +1,15 @@
 'use strict';
 
 const {HttpCode} = require(`./http-code.enum`);
+const {ApiRoute} = require(`./api-route.enum`);
+const {ApiMainRoute} = require(`./api-main-route.enum`);
+const {ApiMyRoute} = require(`./api-my-route.enum`);
+const {ApiArticlesRoute} = require(`./api-articles-route.enum`);
 
 module.exports = {
   HttpCode,
+  ApiRoute,
+  ApiMainRoute,
+  ApiMyRoute,
+  ApiArticlesRoute,
 };
