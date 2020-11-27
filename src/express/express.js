c053e3c2421d4c1c118e48ee59ad4c7788592e93
what1s1ove <zubko.2007@gmail.com>
2020-11-27 08:06:14 +0200

    module3-task1: + api exptress root router

---
+++ src/express/express.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require(`express`);
+const {ApiRoute} = require(`~/common/enums`);
+const mainRouter = require(`~/express/routes/main/main.router`);
+const myRouter = require(`~/express/routes/my/my.router`);
+const articlesRouter = require(`~/express/routes/articles/articles.router`);
+
+const DEFAULT_PORT = 8080;
+
+const app = express();
+
+app.use(ApiRoute.MAIN, mainRouter);
+app.use(ApiRoute.MY, myRouter);
+app.use(ApiRoute.ARTICLES, articlesRouter);
+
+app.listen(DEFAULT_PORT);
