96443bdbe2c391b15d1e0c2f5bcfde5e00c28969
what1s1ove <zubko.2007@gmail.com>
2020-11-27 20:24:18 +0200

    module2-task2: + setup public folder as views root

--- src/express/express.js
+++ src/express/express.js
@@ -1,12 +1,14 @@
 'use strict';
 
 require(`module-alias/register`);
+const path = require(`path`);
 const express = require(`express`);
-const {ApiRoute} = require(`~/common/enums`);
+const {ApiRoute, HttpCode} = require(`~/common/enums`);
 const mainRouter = require(`~/express/routes/main/main.router`);
 const myRouter = require(`~/express/routes/my/my.router`);
 const articlesRouter = require(`~/express/routes/articles/articles.router`);
 
+const PUBLIC_DIR = `public`;
 const DEFAULT_PORT = 8080;
 
 const app = express();
@@ -14,5 +16,17 @@ const app = express();
 app.use(ApiRoute.MAIN, mainRouter);
 app.use(ApiRoute.MY, myRouter);
 app.use(ApiRoute.ARTICLES, articlesRouter);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.use((_, res) =>
+  res.status(HttpCode.BAD_REQUEST).render(`pages/errors/404`)
+);
+app.use((_err, _req, res, _next) =>
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`pages/errors/500`)
+);
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
