e3b16f20fb19d8875a385d5aba099a1fc1d8a1fe
what1s1ove <zubko.2007@gmail.com>
2020-12-12 19:34:40 +0200

    module4-task1: + comments data service

--- src/service/data/index.js
+++ src/service/data/index.js
@@ -3,9 +3,11 @@
 const Search = require(`./search/search`);
 const Category = require(`./category/category`);
 const Articles = require(`./articles/articles`);
+const Comments = require(`./comments/comments`);
 
 module.exports = {
   Search,
   Category,
   Articles,
+  Comments,
 };
