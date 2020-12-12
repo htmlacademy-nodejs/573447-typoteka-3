c2e4e7d7768b25f6f4db5154180ab9763e09721a
what1s1ove <zubko.2007@gmail.com>
2020-12-12 20:12:19 +0200

    module4-task1: * article api path enum imports

--- src/common/enums/api/index.js
+++ src/common/enums/api/index.js
@@ -4,10 +4,12 @@ const {HttpCode} = require(`./http-code.enum`);
 const {ApiPath} = require(`./api-path.enum`);
 const {SearchApiPath} = require(`./search-api-path.enum`);
 const {CategoryApiPath} = require(`./category-api-path.enum`);
+const {ArticlesApiPath} = require(`./articles-api-path.enum`);
 
 module.exports = {
   HttpCode,
   ApiPath,
   SearchApiPath,
   CategoryApiPath,
+  ArticlesApiPath,
 };
