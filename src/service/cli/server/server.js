584813be4502810f6af1cae23377f0e39ff97d5b
what1s1ove <zubko.2007@gmail.com>
2020-12-13 18:36:09 +0200

    module4-task2: * server service

--- src/service/cli/server/server.js
+++ src/service/cli/server/server.js
@@ -2,17 +2,33 @@
 
 const express = require(`express`);
 const apiRouter = require(`~/service/api/api`);
-const {paintMessage} = require(`~/helpers`);
-const {CliCommandName, HttpCode, MessageColor} = require(`~/common/enums`);
+const {getLogger} = require(`~/helpers`);
+const {CliCommandName, HttpCode, LoggerName} = require(`~/common/enums`);
 const {API_PREFIX} = require(`~/common/constants`);
 const {DEFAULT_PORT} = require(`./common`);
 
 const app = express();
+const logger = getLogger({
+  name: LoggerName.API,
+});
+
 
 app.use(express.json());
-app.use(API_PREFIX, apiRouter);
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
 
-app.use((_, res) => res.status(HttpCode.NOT_FOUND).send(`Not found`));
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+
+  return next();
+});
+app.use(API_PREFIX, apiRouter);
+app.use((req, res) => {
+  logger.error(`Route not found: ${req.url}`);
+
+  return res.status(HttpCode.NOT_FOUND).send(`Not found`);
+});
 
 module.exports = {
   name: CliCommandName.SERVER,
@@ -21,11 +37,13 @@ module.exports = {
     const port = Number(customPort) || DEFAULT_PORT;
 
     const server = app.listen(port, () => {
-      console.info(paintMessage(`Ожидаю соединений на ${port}`, MessageColor.GREEN));
+      return logger.info(`Listening to connections on ${port}`);
     });
 
     server.once(`error`, (err) => {
-      console.error(`Ошибка при создании сервера`, err);
+      return logger.error(
+          `An error occured on server creation: ${err.message}`,
+      );
     });
   },
 };
