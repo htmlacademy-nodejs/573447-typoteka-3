7546d820203a6ef94b9e98bf32f3b3ceb98fc8a9
Vohmyanin Sergey Vasilevich <delagen@gmail.com>
2020-12-14 21:07:56 +0300

    fix some logger configuration

--- src/helpers/log/get-logger.helper.js
+++ src/helpers/log/get-logger.helper.js
@@ -4,7 +4,7 @@ const pino = require(`pino`);
 const {AppEnvironment, ENV, LogLevel, LoggerName} = require(`~/common/enums`);
 
 const LOG_FILE = `./logs/api.log`;
-const isDevMode = ENV.NODE_ENV === AppEnvironment.DEVELOPMENT;
+const isDevMode = ENV.NODE_ENV !== AppEnvironment.PRODUCTION;
 const defaultLogLevel = isDevMode ? LogLevel.INFO : LogLevel.ERROR;
 
 const logger = pino(
