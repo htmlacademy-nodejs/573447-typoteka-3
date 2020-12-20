ecd0794bb76f2069f9b2e0c656040445bb7bacda
what1s1ove <zubko.2007@gmail.com>
2020-12-20 12:20:54 +0200

    module4-task3: * search router

--- src/express/routes/main/main.router.js
+++ src/express/routes/main/main.router.js
@@ -12,7 +12,7 @@ const initMainRouter = (app, settings) => {
   mainRouter.get(SsrMainPath.ROOT, async (_, res) => {
     const articles = await api.getArticles();
 
-    res.render(`pages/main`, {
+    return res.render(`pages/main`, {
       previews: articles,
       title: `Типотека`,
       hiddenTitle: ` Главная страница личного блога Типотека`,
@@ -25,31 +25,32 @@ const initMainRouter = (app, settings) => {
   });
 
   mainRouter.get(SsrMainPath.REGISTER, (_, res) => {
-    const content = {
+    return res.render(`pages/register`, {
       title: `Типотека`,
       error: {
         email: false,
         password: false,
       },
-    };
-
-    res.render(`pages/register`, content);
+    });
   });
 
   mainRouter.get(SsrMainPath.LOGIN, (_, res) => {
-    const content = {
+    return res.render(`pages/login`, {
       title: `Типотека`,
       error: {
         email: false,
         password: false,
       },
-    };
-
-    res.render(`pages/login`, content);
+    });
   });
 
-  mainRouter.get(SsrMainPath.SEARCH, (_, res) => {
-    const content = {
+  mainRouter.get(SsrMainPath.SEARCH, async (req, res) => {
+    const {search} = req.query;
+    const results = await api.search(search);
+
+    return res.render(`pages/search`, {
+      results,
+      searchValue: search,
       title: `Типотека`,
       hiddenTitle: ` Страница поиска личного блога Типотека`,
       account: {
@@ -57,38 +58,8 @@ const initMainRouter = (app, settings) => {
         name: `Алёна Фролова`,
         avatar: `img/avatar-2.png`,
       },
-      isResult: false,
-      searchResult: {
-        type: `list`,
-        list: [
-          {
-            date: {
-              stamp: `2019-03-21T20:33`,
-              day: `21.03.2019`,
-              time: `20:33`,
-            },
-            link: {
-              text: `Huawei открыла в России путешествия на смартфон Mate 30 Pro без сервисов Google`,
-              href: `#`,
-            },
-          },
-          {
-            date: {
-              stamp: `2019-03-21T20:33`,
-              day: `21.03.2019`,
-              time: `20:33`,
-            },
-            link: {
-              text: `«Яндекс.Метрика» запустила путешествия сервис для оценки эффективности баннеров и видеорекламы в реальном времени`,
-              href: `#`,
-            },
-          },
-        ],
-      },
       scriptList: [`js/main.js`],
-    };
-
-    res.render(`pages/search`, content);
+    });
   });
 };
 
