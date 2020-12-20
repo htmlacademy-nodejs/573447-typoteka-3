4d35591c0575bb0d90fff580eb78d46efd8760d1
what1s1ove <zubko.2007@gmail.com>
2020-12-20 11:26:11 +0200

    module4-task3: * app initialing

--- src/express/express.js
+++ src/express/express.js
@@ -3,30 +3,40 @@
 require(`module-alias/register`);
 const path = require(`path`);
 const express = require(`express`);
-const {SsrPath, HttpCode, ENV} = require(`~/common/enums`);
-const mainRouter = require(`~/express/routes/main/main.router`);
+const {Api} = require(`~/express/services`);
+const {SsrPath, HttpCode} = require(`~/common/enums`);
+const {initMainRouter} = require(`~/express/routes/main/main.router`);
 const myRouter = require(`~/express/routes/my/my.router`);
 const articlesRouter = require(`~/express/routes/articles/articles.router`);
-
-const PUBLIC_DIR = `public`;
-const DEFAULT_PORT = 8080;
+const {AppConfig} = require(`./common`);
 
 const app = express();
+const api = new Api({
+  baseURL: AppConfig.API_URL,
+  timeout: AppConfig.API_TIMEOUT,
+});
+const routerSettings = {
+  api,
+};
+const routerInits = [initMainRouter];
 
-app.use(SsrPath.MAIN, mainRouter);
+routerInits.forEach((initRouter) => {
+  initRouter(app, routerSettings);
+});
+
 app.use(SsrPath.MY, myRouter);
 app.use(SsrPath.ARTICLES, articlesRouter);
 
-app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.use(express.static(path.resolve(__dirname, AppConfig.PUBLIC_DIR)));
 
 app.use((_, res) =>
   res.status(HttpCode.BAD_REQUEST).render(`pages/errors/404`)
 );
-app.use((_err, _req, res, _next) =>
+app.use((_err, _req, res, _next) => (
   res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`pages/errors/500`)
-);
+));
 
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
 
-app.listen(ENV.PORT || DEFAULT_PORT);
+app.listen(AppConfig.DEFAULT_PORT);
