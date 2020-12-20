535d83812d3759f28e93a9e48b3838b2be80e259
what1s1ove <zubko.2007@gmail.com>
2020-12-20 12:33:01 +0200

    module4-task3: + output my page

--- src/express/routes/my/my.router.js
+++ src/express/routes/my/my.router.js
@@ -1,108 +1,87 @@
 'use strict';
 
 const {Router} = require(`express`);
-const {SsrMyPath} = require(`~/common/enums`);
+const {SsrPath, SsrMyPath} = require(`~/common/enums`);
 
-const myRouter = new Router();
+const initMyRouter = (app, settings) => {
+  const myRouter = new Router();
+  const {api} = settings;
 
-myRouter.get(SsrMyPath.ROOT, (_, res) => {
-  const content = {
-    articleList: [
-      {
-        id: 1,
-        date: {
-          stamp: `2019-03-21T20:33`,
-          day: `21.03.2019`,
-          time: `20:33`,
-        },
-        title: `AirPods в один клик`,
-      },
-      {
-        id: 2,
-        date: {
-          stamp: `2019-03-21T20:33`,
-          day: `21.03.2019`,
-          time: `20:33`,
-        },
-        title: `AirPods в один клик`,
-      },
-      {
-        id: 3,
-        date: {
-          stamp: `2019-03-21T20:33`,
-          day: `21.03.2019`,
-          time: `20:33`,
-        },
-        title: `AirPods в один клик`,
-      },
-    ],
-  };
+  app.use(SsrPath.MY, myRouter);
 
-  res.render(`pages/my/my`, content);
-});
-myRouter.get(SsrMyPath.COMMENTS, (_, res) => {
-  const content = {
-    commentList: [
-      {
-        account: {
-          type: `user`,
-          avatar: `img/avatar-1.png`,
-          name: `Евгений Петров`,
-        },
-        date: {
-          stamp: `2019-03-21T20:33`,
-          day: `21.03.2019`,
-          time: `20:33`,
-        },
-        text: `Автор, ты все выдумал, покайся`,
-        articleTitle: `AirPods в один клик`,
-      },
-      {
-        account: {
-          type: `user`,
-          avatar: `img/avatar-5.png`,
-          name: `Александр Марков`,
-        },
-        date: {
-          stamp: `2019-03-21T20:33`,
-          day: `21.03.2019`,
-          time: `20:33`,
-        },
-        text: `Конечно, прежде чем так писать, нужно искренне приложить усилия, чтобы разобраться — не все люди умеют выражать свои мысли.`,
-        articleTitle: `AirPods в один клик`,
-      },
-      {
-        account: {
-          type: `user`,
-          avatar: `img/avatar-4.png`,
-          name: `Евгений Петров`,
-        },
-        date: {
-          stamp: `2019-03-21T20:33`,
-          day: `21.03.2019`,
-          time: `20:33`,
-        },
-        text: `Автор, ты все выдумал, покайся`,
-        articleTitle: `AirPods в один клик`,
-      },
-      {
-        account: {
-          type: `user`,
-          avatar: `img/avatar-3.png`,
-          name: `Александр Марков`,
-        },
-        date: {
-          stamp: `2019-03-21T20:33`,
-          day: `21.03.2019`,
-          time: `20:33`,
-        },
-        text: `Конечно, прежде чем так писать, нужно искренне приложить усилия, чтобы разобраться — не все люди умеют выражать свои мысли.`,
-        articleTitle: `AirPods в один клик`,
-      },
-    ],
-  };
+  myRouter.get(SsrMyPath.ROOT, async (_, res) => {
+    const articles = await api.getArticles();
 
-  res.render(`pages/my/comments`, content);
-});
+    return res.render(`pages/my/my`, {
+      scriptList: [`js/main.js`],
+      articleList: articles,
+    });
+  });
 
-module.exports = myRouter;
+  myRouter.get(SsrMyPath.COMMENTS, (_, res) => {
+    return res.render(`pages/my/comments`, {
+      commentList: [
+        {
+          account: {
+            type: `user`,
+            avatar: `img/avatar-1.png`,
+            name: `Евгений Петров`,
+          },
+          date: {
+            stamp: `2019-03-21T20:33`,
+            day: `21.03.2019`,
+            time: `20:33`,
+          },
+          text: `Автор, ты все выдумал, покайся`,
+          articleTitle: `AirPods в один клик`,
+        },
+        {
+          account: {
+            type: `user`,
+            avatar: `img/avatar-5.png`,
+            name: `Александр Марков`,
+          },
+          date: {
+            stamp: `2019-03-21T20:33`,
+            day: `21.03.2019`,
+            time: `20:33`,
+          },
+          text: `Конечно, прежде чем так писать, нужно искренне приложить усилия, чтобы разобраться — не все люди умеют выражать свои мысли.`,
+          articleTitle: `AirPods в один клик`,
+        },
+        {
+          account: {
+            type: `user`,
+            avatar: `img/avatar-4.png`,
+            name: `Евгений Петров`,
+          },
+          date: {
+            stamp: `2019-03-21T20:33`,
+            day: `21.03.2019`,
+            time: `20:33`,
+          },
+          text: `Автор, ты все выдумал, покайся`,
+          articleTitle: `AirPods в один клик`,
+        },
+        {
+          account: {
+            type: `user`,
+            avatar: `img/avatar-3.png`,
+            name: `Александр Марков`,
+          },
+          date: {
+            stamp: `2019-03-21T20:33`,
+            day: `21.03.2019`,
+            time: `20:33`,
+          },
+          text: `Конечно, прежде чем так писать, нужно искренне приложить усилия, чтобы разобраться — не все люди умеют выражать свои мысли.`,
+          articleTitle: `AirPods в один клик`,
+        },
+      ],
+    });
+  });
+};
+
+module.exports = {
+  initMyRouter,
+};
