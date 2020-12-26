d79f9597fb5ca526f04cb9938de0df15dd41b9ca
what1s1ove <zubko.2007@gmail.com>
2020-12-26 17:54:41 +0200

    module5-task1: + create comments table query

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS articles;
+DROP TABLE IF EXISTS comments;
 
 CREATE TABLE users
 (
@@ -31,4 +32,16 @@ CREATE TABLE articles
   FOREIGN KEY (user_id) REFERENCES users (id)
     ON UPDATE CASCADE
     ON DELETE CASCADE
-)
+);
+
+CREATE TABLE comments
+(
+  id SERIAL PRIMARY KEY,
+  text VARCHAR(1000) NOT NULL,
+  created_date DATE NOT NULL,
+
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
