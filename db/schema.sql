6e466be3d0c729656036448de20f91db5e0b3172
what1s1ove <zubko.2007@gmail.com>
2020-12-26 17:43:53 +0200

    module5-task1: + create user table query

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  picture VARCHAR(50) NOT NULL
+);
