b1fd0e89123afae586d0d513da3218781b476003
what1s1ove <zubko.2007@gmail.com>
2020-12-27 19:35:14 +0200

    module5-task2: + offer constants

---
+++ src/common/constants/array.constants.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const INCREASE_COUNT_FOR_IDX = 1;
+const INITIAL_ARRAY_IDX = 0;
+
+module.exports = {
+  INCREASE_COUNT_FOR_IDX,
+  INITIAL_ARRAY_IDX,
+};
--- src/common/constants/index.js
+++ src/common/constants/index.js
@@ -3,10 +3,16 @@
 const {USER_ARGV_IDX, COMMAND_ARGS_IDX} = require(`./cli.constants`);
 const {MOCKS_FILE_PATH} = require(`./mocks.constants`);
 const {API_PREFIX} = require(`./api.constants`);
+const {
+  INITIAL_ARRAY_IDX,
+  INCREASE_COUNT_FOR_IDX,
+} = require(`./array.constants`);
 
 module.exports = {
   USER_ARGV_IDX,
   COMMAND_ARGS_IDX,
   MOCKS_FILE_PATH,
   API_PREFIX,
+  INITIAL_ARRAY_IDX,
+  INCREASE_COUNT_FOR_IDX,
 };
