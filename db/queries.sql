5ea7de3115234ee2130616df7b5cbe1b3938888c
what1s1ove <zubko.2007@gmail.com>
2020-12-31 08:11:49 +0200

    module5-task3: + categories with offers query

--- db/queries.sql
+++ db/queries.sql
@@ -1,2 +1,8 @@
 /* All categories */
 SELECT * FROM categories;
+
+/* Categories with at least one article  */
+SELECT * FROM categories WHERE id IN
+(
+  SELECT category_id FROM articles_categories
+);
