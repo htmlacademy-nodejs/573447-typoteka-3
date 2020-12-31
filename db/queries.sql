12a479d5fcca50945ced67917cf5999754c64973
what1s1ove <zubko.2007@gmail.com>
2020-12-31 08:13:37 +0200

    module5-task3: + categories with articlrs count

--- db/queries.sql
+++ db/queries.sql
@@ -6,3 +6,16 @@ SELECT * FROM categories WHERE id IN
 (
   SELECT category_id FROM articles_categories
 );
+
+/* Categories with articles count */
+SELECT
+  categories.name,
+  articles_categories.category_id,
+  count(articles_categories.article_id) AS "articles_count"
+FROM
+  articles_categories
+  INNER JOIN categories
+    ON articles_categories.category_id = categories.id
+GROUP BY
+  articles_categories.category_id,
+  categories.name;
