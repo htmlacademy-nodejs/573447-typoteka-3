afe6a92a1a04d67d01d29f5b2b194d7ae6df5fed
what1s1ove <zubko.2007@gmail.com>
2021-01-03 12:17:34 +0200

    module5-task3: * app env

--- src/common/enums/app/env.enum.js
+++ src/common/enums/app/env.enum.js
@@ -1,14 +1,29 @@
 'use strict';
 
-const dotenv = require(`dotenv`);
+const {AppEnvironment} = require(`~/common/enums/app`);
 
-dotenv.config();
+const {
+  NODE_ENV,
+  LOG_LEVEL,
+  API_PORT,
+  PORT,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+} = process.env;
 
 const ENV = {
-  NODE_ENV: process.env.NODE_ENV,
-  LOG_LEVEL: process.env.LOG_LEVEL,
-  API_PORT: process.env.API_PORT,
-  PORT: process.env.PORT,
+  NODE_ENV: NODE_ENV || AppEnvironment.DEVELOPMENT,
+  LOG_LEVEL: LOG_LEVEL || ``,
+  API_PORT: API_PORT || ``,
+  PORT: PORT || ``,
+  DB_HOST: DB_HOST || ``,
+  DB_PORT: DB_PORT || ``,
+  DB_NAME: DB_NAME || ``,
+  DB_USER: DB_USER || ``,
+  DB_PASSWORD: DB_PASSWORD || ``,
 };
 
 module.exports = {
