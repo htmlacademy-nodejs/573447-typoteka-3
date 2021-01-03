97fba7cd3f514e839619f852d9e43721bf72f424
what1s1ove <zubko.2007@gmail.com>
2021-01-03 12:31:17 +0200

    module6-task1: - sequelize config

---
+++ src/service/db/sequelize-config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const {AppEnvironment, ENV} = require(`~/common/enums`);
+
+const dbConfig = {
+  [AppEnvironment.DEVELOPMENT]: {
+    database: ENV.DB_NAME,
+    host: ENV.DB_HOST,
+    port: ENV.DB_PORT,
+    username: ENV.DB_USER,
+    password: ENV.DB_PASSWORD,
+  },
+  [AppEnvironment.PRODUCTION]: {
+    database: ENV.DB_NAME,
+    host: ENV.DB_HOST,
+    port: ENV.DB_PORT,
+    username: ENV.DB_USER,
+    password: ENV.DB_PASSWORD,
+  },
+};
+
+module.exports = {
+  dbConfig,
+};
