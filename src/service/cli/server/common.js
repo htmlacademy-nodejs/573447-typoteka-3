f09d72801baa7521025b3666846957802fe323a0
what1s1ove <zubko.2007@gmail.com>
2021-01-03 12:28:18 +0200

    module6-task1: - server constants

--- src/service/cli/server/common.js
+++ src/service/cli/server/common.js
@@ -2,14 +2,6 @@
 
 const DEFAULT_PORT = 3000;
 
-const MOCKS_FILE_PATH = `mocks.json`;
-
-const ApiPath = {
-  POSTS: `/posts`,
-};
-
 module.exports = {
   DEFAULT_PORT,
-  MOCKS_FILE_PATH,
-  ApiPath,
 };
