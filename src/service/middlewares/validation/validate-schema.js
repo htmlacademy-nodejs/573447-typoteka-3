a030b57399b9561ec2fa8cec8b683b7d05e3e8eb
what1s1ove <zubko.2007@gmail.com>
2021-01-21 11:36:48 +0200

    module7-task1: + validation middleware

---
+++ src/service/middlewares/validation/validate-schema.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const {ValidationError} = require(`joi`);
+const {HttpCode} = require(`~/common/enums`);
+
+const validateSchema = (schema) => async (req, res, next) => {
+  const {body} = req;
+
+  try {
+    await schema.validateAsync(body, {
+      abortEarly: false,
+      convert: false,
+    });
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      const {details} = err;
+
+      return res.status(HttpCode.BAD_REQUEST).send({
+        message: details.map((error) => error.message),
+      });
+    }
+  }
+
+  return next();
+};
+
+module.exports = {
+  validateSchema,
+};
