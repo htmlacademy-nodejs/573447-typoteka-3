85dc2cbeb29bc167a286c8779122a3de6e6c57a6
what1s1ove <zubko.2007@gmail.com>
2021-01-22 20:42:49 +0200

    module7-task1: + async handler to the ssr routes

--- src/express/routes/articles/articles.router.js
+++ src/express/routes/articles/articles.router.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const {Router} = require(`express`);
-const {getHttpErrors} = require(`~/helpers`);
+const {getHttpErrors, asyncHandler} = require(`~/helpers`);
 const {SsrPath, SsrArticlePath} = require(`~/common/enums`);
 const {getArticleData} = require(`./helpers`);
 
@@ -11,39 +11,45 @@ const initArticlesRouter = (app, settings) => {
 
   app.use(SsrPath.ARTICLES, articlesRouter);
 
-  articlesRouter.get(SsrArticlePath.EDIT_$ARTICLE_ID, async (req, res) => {
-    const {id} = req.params;
-    const [article, categories] = await Promise.all([
-      api.getArticle(id),
-      api.getCategories(),
-    ]);
+  articlesRouter.get(
+      SsrArticlePath.EDIT_$ARTICLE_ID,
+      asyncHandler(async (req, res) => {
+        const {id} = req.params;
+        const [article, categories] = await Promise.all([
+          api.getArticle(id),
+          api.getCategories(),
+        ]);
 
-    return res.render(`pages/articles/edit`, {
-      article,
-      categories,
-      account: {
-        type: `admin`,
-      },
-    });
-  });
+        return res.render(`pages/articles/edit`, {
+          article,
+          categories,
+          account: {
+            type: `admin`,
+          },
+        });
+      })
+  );
 
-  articlesRouter.get(SsrArticlePath.ADD, async (_req, res) => {
-    const categories = await api.getCategories();
+  articlesRouter.get(
+      SsrArticlePath.ADD,
+      asyncHandler(async (_req, res) => {
+        const categories = await api.getCategories();
 
-    return res.render(`pages/articles/edit`, {
-      categories,
-      article: {},
-      account: {
-        type: `admin`,
-      },
-    });
-  });
+        return res.render(`pages/articles/edit`, {
+          categories,
+          article: {},
+          account: {
+            type: `admin`,
+          },
+        });
+      })
+  );
 
 
   articlesRouter.post(
       SsrArticlePath.ADD,
       storage.upload.single(`avatar`),
-      async (req, res) => {
+      asyncHandler(async (req, res) => {
         const {body, file} = req;
         const articleData = getArticleData(body, file);
 
@@ -63,13 +69,13 @@ const initArticlesRouter = (app, settings) => {
             },
           });
         }
-      }
+      })
   );
 
   articlesRouter.post(
       SsrArticlePath.EDIT_$ARTICLE_ID,
       storage.upload.single(`avatar`),
-      async (req, res) => {
+      asyncHandler(async (req, res) => {
         const {body, file, params} = req;
         const {id} = params;
         const parsedId = Number(id);
@@ -87,7 +93,7 @@ const initArticlesRouter = (app, settings) => {
             categories,
             article: {
               ...article,
-              ...articleData
+              ...articleData,
             },
             errorMessages: getHttpErrors(err),
             account: {
@@ -95,69 +101,78 @@ const initArticlesRouter = (app, settings) => {
             },
           });
         }
-      },
+      })
   );
 
-  articlesRouter.get(SsrArticlePath.$ARTICLE_ID, async (req, res) => {
-    const {id} = req.params;
-    const [article, categories] = await Promise.all([
-      api.getArticle(id),
-      api.getCategories(),
-    ]);
+  articlesRouter.get(
+      SsrArticlePath.$ARTICLE_ID,
+      asyncHandler(async (req, res) => {
+        const {id} = req.params;
+        const [article, categories] = await Promise.all([
+          api.getArticle(id),
+          api.getCategories(),
+        ]);
 
-    return res.render(`pages/articles/article`, {
-      article,
-      themes: categories,
-      account: {
-        type: `user`,
-        name: `Алёна Фролова`,
-        avatar: `img/avatar-2.png`,
-      },
-    });
-  });
+        return res.render(`pages/articles/article`, {
+          article,
+          themes: categories,
+          account: {
+            type: `user`,
+            name: `Алёна Фролова`,
+            avatar: `img/avatar-2.png`,
+          },
+        });
+      })
+  );
 
-  articlesRouter.get(SsrArticlePath.CATEGORY_$ARTICLE_ID, (_req, res) => {
-    return res.render(`pages/articles/categories`, {
-      title: `Типотека`,
-      displayedTitle: `Бизнес`,
-      description: `Это приветственный текст, который владелец блога может выбрать, чтобы описать себя 👏`,
-      account: {
-        type: `user`,
-        name: `Алёна Фролова`,
-        avatar: `img/avatar-2.png`,
-      },
-      hasContent: true,
-      hasHot: true,
-      hasLastComments: true,
-    });
-  });
+  articlesRouter.get(
+      SsrArticlePath.CATEGORY_$ARTICLE_ID,
+      asyncHandler((_req, res) => {
+        return res.render(`pages/articles/categories`, {
+          title: `Типотека`,
+          displayedTitle: `Бизнес`,
+          description: `Это приветственный текст, который владелец блога может выбрать, чтобы описать себя 👏`,
+          account: {
+            type: `user`,
+            name: `Алёна Фролова`,
+            avatar: `img/avatar-2.png`,
+          },
+          hasContent: true,
+          hasHot: true,
+          hasLastComments: true,
+        });
+      })
+  );
 
-  articlesRouter.post(SsrArticlePath.$ARTICLE_ID_COMMENT, async (req, res) => {
-    const {body, params} = req;
-    const parsedComment = Number(params.id);
+  articlesRouter.post(
+      SsrArticlePath.$ARTICLE_ID_COMMENT,
+      asyncHandler(async (req, res) => {
+        const {body, params} = req;
+        const parsedComment = Number(params.id);
 
-    try {
-      await api.createComment(parsedComment, {
-        text: body.comment,
-      });
+        try {
+          await api.createComment(parsedComment, {
+            text: body.comment,
+          });
 
-      return res.redirect(`${SsrPath.ARTICLES}/${parsedComment}`);
-    } catch (err) {
-      const categories = await api.getCategories();
-      const article = await api.getArticle(parsedComment);
+          return res.redirect(`${SsrPath.ARTICLES}/${parsedComment}`);
+        } catch (err) {
+          const categories = await api.getCategories();
+          const article = await api.getArticle(parsedComment);
 
-      return res.render(`pages/articles/article`, {
-        article,
-        themes: categories,
-        errorMessages: getHttpErrors(err),
-        account: {
-          type: `user`,
-          name: `Алёна Фролова`,
-          avatar: `img/avatar-2.png`,
-        },
-      });
-    }
-  });
+          return res.render(`pages/articles/article`, {
+            article,
+            themes: categories,
+            errorMessages: getHttpErrors(err),
+            account: {
+              type: `user`,
+              name: `Алёна Фролова`,
+              avatar: `img/avatar-2.png`,
+            },
+          });
+        }
+      })
+  );
 };
 
 module.exports = {
