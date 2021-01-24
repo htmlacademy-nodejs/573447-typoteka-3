7f1825e1223fd1b5f0bdf0774c51510795fa0826
what1s1ove <zubko.2007@gmail.com>
2021-01-24 10:43:07 +0200

    module7-task2: * move validartion meddlewares

--- src/service/middlewares/validation/index.js
+++ src/service/middlewares/validation/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const {validateSchema} = require(`./validate-schema`);
-const {validateParamSchema} = require(`./validate-param-schema`);
+const {validateSchema} = require(`./validate-schema/validate-schema`);
+const {validateParamSchema} = require(`./validate-param-schema/validate-param-schema`);
 
 module.exports = {
   validateSchema,
