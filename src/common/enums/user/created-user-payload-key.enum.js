cb749a2217dd9f09b162e1fb6fca051440d21cc4
what1s1ove <zubko.2007@gmail.com>
2021-01-24 10:35:51 +0200

    module7-task2: + created user payload keys

---
+++ src/common/enums/user/created-user-payload-key.enum.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const CreatedUserPayloadKey = {
+  EMAIL: `email`,
+  PASSWORD: `password`,
+  REPEATED_PASSWORD: `repeatedPassword`,
+  FIRST_NAME: `firstName`,
+  LAST_NAME: `lastName`,
+  AVATAR: `avatar`,
+};
+
+module.exports = {
+  CreatedUserPayloadKey,
+};
--- src/common/enums/user/index.js
+++ src/common/enums/user/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const {UserKey} = require(`./user-key.enum`);
+const {CreatedUserPayloadKey} = require(`./created-user-payload-key.enum`);
 
 module.exports = {
   UserKey,
+  CreatedUserPayloadKey,
 };
