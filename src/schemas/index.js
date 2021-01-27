cb16a6b2247cd1555bc49ef2ef66ce3d890abc79
what1s1ove <zubko.2007@gmail.com>
2021-01-27 12:27:45 +0200

    module7-task2: * http code enum

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,11 +3,12 @@
 const {comment} = require(`./comment/comment.schema`);
 const {article} = require(`./article/article.schema`);
 const {routeId} = require(`./route`);
-const {createdUserPayload} = require(`./user`);
+const {createdUserPayload, userLoginPayload} = require(`./user`);
 
 module.exports = {
   comment,
   article,
   routeId,
   createdUserPayload,
+  userLoginPayload,
 };
