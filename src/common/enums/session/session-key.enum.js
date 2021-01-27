aab9a617938cea9449ca232e1b3a5621585acc14
what1s1ove <zubko.2007@gmail.com>
2021-01-27 12:42:25 +0200

    module7-task2: + session key enum

---
+++ src/common/enums/session/session-key.enum.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const SessionKey = {
+  SID: `sid`,
+  EXPIRES: `expires`,
+  DATA: `data`,
+  CREATED_AT: `createdAt`,
+  UPDATED_AT: `updatedAt`,
+};
+
+module.exports = {
+  SessionKey,
+};
