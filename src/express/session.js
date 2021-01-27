68a89ee73e512cec96fa87f0621a0ef36e7ad08d
what1s1ove <zubko.2007@gmail.com>
2021-01-27 12:55:09 +0200

    module7-task3: + session middleware

---
+++ src/express/session.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const session = require(`express-session`);
+const SequelizeStore = require(`connect-session-sequelize`);
+const sequelize = require(`~/db/db`);
+const {ENV, TableName, SessionExpiration} = require(`~/common/enums`);
+
+const SequelizeStoreInstance = new SequelizeStore(session.Store);
+
+const appSessionStore = new SequelizeStoreInstance({
+  db: sequelize,
+  expiration: SessionExpiration.PERIOD,
+  checkExpirationInterval: SessionExpiration.CHECK,
+  tableName: TableName.SESSIONS,
+});
+
+const sessionMiddleware = session({
+  secret: ENV.SECRET,
+  store: appSessionStore,
+  resave: false,
+  proxy: true,
+  saveUninitialized: false,
+  name: `session_id`,
+});
+
+module.exports = {
+  sessionMiddleware,
+};
