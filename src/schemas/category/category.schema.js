10aca10af50556ac4609bf643685eb021c2f3c22
what1s1ove <zubko.2007@gmail.com>
2021-02-02 20:56:01 +0200

    module8-task2: * category validation schema

--- src/schemas/category/category.schema.js
+++ src/schemas/category/category.schema.js
@@ -1,14 +1,22 @@
 'use strict';
 
 const Joi = require(`joi`);
-const {CategoryKey, CategoryValidationMessage} = require(`~/common/enums`);
+const {
+  CategoryKey,
+  CategoryValidationRule,
+  CategoryValidationMessage,
+} = require(`~/common/enums`);
 
 const category = Joi.object({
-  [CategoryKey.NAME]: Joi.string({
-    'string.min': CategoryValidationMessage.NAME_MIN_LENGTH,
-    'string.max': CategoryValidationMessage.NAME_MAX_LENGTH,
-    'any.required': CategoryValidationMessage.NAME_REQUIRE,
-  }),
+  [CategoryKey.NAME]: Joi.string()
+    .min(CategoryValidationRule.NAME_MIN_LENGTH)
+    .max(CategoryValidationRule.NAME_MAX_LENGTH)
+    .required()
+    .messages({
+      'string.min': CategoryValidationMessage.NAME_MIN_LENGTH,
+      'string.max': CategoryValidationMessage.NAME_MAX_LENGTH,
+      'any.required': CategoryValidationMessage.NAME_REQUIRE,
+    }),
 });
 
 module.exports = {
