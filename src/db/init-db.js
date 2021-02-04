0bf3eab98a196b8ffa57b60aa5cd61507a51d91c
what1s1ove <zubko.2007@gmail.com>
2021-02-04 20:44:56 +0200

    module8-task2: * init db helper

--- src/db/init-db.js
+++ src/db/init-db.js
@@ -1,45 +1,28 @@
 'use strict';
 
 const {defineModels} = require(`~/db/define-models`);
-const {ModelAlias} = require(`~/common/enums`);
 
 const initDb = async (sequelize, mockedPayload) => {
-  const {categories, articles, users} = mockedPayload;
-  const {Category, Article, User} = defineModels(sequelize);
+  const {
+    categories,
+    articles,
+    users,
+    comments,
+    articlesCategories,
+  } = mockedPayload;
+  const {Category, Article, User, Comment, ArticleCategory} = defineModels(
+      sequelize
+  );
 
   await sequelize.sync({
     force: true,
   });
 
-  const categoryModels = await Category.bulkCreate(
-      categories.map((item) => ({
-        name: item,
-      }))
-  );
-
-  const categoryIdByName = categoryModels.reduce(
-      (acc, next) => ({
-        ...acc,
-        [next.name]: next.id,
-      }),
-      {}
-  );
-
-  for await (const user of users) {
-    await User.create(user);
-  }
-
-  await Promise.all(
-      articles.map(async (article) => {
-        const articleModel = await Article.create(article, {
-          include: [ModelAlias.COMMENTS],
-        });
-
-        await articleModel.addCategories(
-            article.categories.map((name) => categoryIdByName[name])
-        );
-      })
-  );
+  await Category.bulkCreate(categories);
+  await User.bulkCreate(users);
+  await Article.bulkCreate(articles);
+  await Comment.bulkCreate(comments);
+  await ArticleCategory.bulkCreate(articlesCategories);
 };
 
 module.exports = {
